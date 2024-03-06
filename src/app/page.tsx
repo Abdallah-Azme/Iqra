b58a2ad015d29077160743995345ddf3d5884e76
Abdallah-Azme <abdallahazme199794@gmail.com>
2024-03-06 22:57:26 +0200

    building the each soura page, and redesign the home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,8 +3,7 @@ import Hero from "@/components/commen/hero";
 import { randomPageNumber } from "@/utils/random-page";
 
 export default async function Home() {
-  const pageNumber = randomPageNumber();
-  const data = await getRandomAyah(pageNumber);
+  const data = await getRandomAyah(randomPageNumber());
   const ayat = data.verses;
   console.log(ayat);
   return (
